67ef4261983ae462c2b64e796a9aee69ade7fdd2
Jared McQueen <jaredmcqueen@gmail.com>
2021-08-05 14:31:20 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -71,5 +71,5 @@ while True:
 
     logging.info(f"inserted {len(issues)} documents into elasticsearch")
 
-    logging.info("sleeping for 1 minute")
+    logging.info("sleeping for 60 seconds")
     time.sleep(60)
